9a6512f4a1185e7d4021f2568347423857520a0d
Byell127 <anthonybyell@gmail.com>
2025-01-25 19:16:50 +0000

    Fiz uma variavel para colocar o numero de estados e cidades

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,16 @@ float area;
 float PIB;
 float turismo;
 char nome [50];
+int cidade;
+int estado;
 
   printf("Digite o nome do seu país \n");
   scanf("%s", &nome);
     
+    
+
+
+
+
     return 0;
 }
