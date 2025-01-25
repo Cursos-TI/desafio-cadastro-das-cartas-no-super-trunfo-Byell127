74b0268bed2cf86bb07b850da843529b2fe6d6f8
Byell127 <anthonybyell@gmail.com>
2025-01-25 19:08:39 +0000

    coloquei a variavel char

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,15 +2,14 @@
 
 int main() {
     
-float população;
+float habitantes;
 float area;
 float PIB;
 float turismo;
-char nome;
+char nome [50];
 
-
-
-
-
+  printf("Digite o nome do seu país \n");
+  scanf("%s", &nome);
+    
     return 0;
 }
