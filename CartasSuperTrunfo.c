6c65dc72a8c424c90e97c0bbe4ee7acc757bd5a7
Byell127 <anthonybyell@gmail.com>
2025-01-28 18:20:19 +0000

    Criação de mais variaveis para poder distinguir os valores de cada cidade, uma contextualização  sobre a quantidade de estados e cidades cada pais tem, melhoria a exposição de dados de cada cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,19 +7,30 @@ float area;
 float PIB;
 int turismo;
 char nome [50];
-int cidade;
-int estado;
+
+float habitantes2;
+float area2;
+float PIB2;
+int turismo2;
+
+float habitantes3;
+float area3;
+float PIB3;
+int turismo3;
+
+float habitantes4;
+float area4;
+float PIB4;
+int turismo4;
 
   printf("Digite o nome do seu país: \n");
   scanf("%s", &nome);
 
-  printf("Digite seu número de estados: \n");
-  scanf("%d", &estado);   
+  printf("Seu país tem 8 estados com cada um obtendo 4 cidades. \n");
+  printf("\n");
     
-printf("Digite o numero de cidades de cada estado: \n");
-  scanf("%d", &cidade); 
-
 printf("Vamos começar com o estado A... \n");
+printf("\n");
 
 printf("Informe a População da cidade A01: \n");
 scanf("%f", &habitantes);
@@ -36,44 +47,44 @@ scanf("%d", &turismo);
 printf("Vamos para a cidade A02: \n");
 
 printf("Informe a População da cidade A02 : \n");
-scanf("%f", &habitantes);
+scanf("%f", &habitantes2);
 
 printf("Informe Área da cidade A02: \n");
-scanf("%f", &area);
+scanf("%f", &area2);
 
 printf("Informe o PIB da cidade A02: \n");
-scanf("%f", &PIB);
+scanf("%f", &PIB2);
 
 printf("Informe a quantidade de pontos turisticos da cidade A02: \n");
-scanf("%d", &turismo);
+scanf("%d", &turismo2);
 
 printf("Vamos para a cidade A03: \n");
 
 printf("Informe a População da cidade A03 : \n");
-scanf("%f", &habitantes);
+scanf("%f", &habitantes3);
 
 printf("Informe Área da cidade A03: \n");
-scanf("%f", &area);
+scanf("%f", &area3);
 
 printf("Informe o PIB da cidade A03: \n");
-scanf("%f", &PIB);
+scanf("%f", &PIB3);
 
 printf("Informe a quantidade de pontos turisticos da cidade A03: \n");
-scanf("%d", &turismo);
+scanf("%d", &turismo3);
 
 printf("Vamos para a cidade A04: \n");
 
 printf("Informe a População da cidade A04 : \n");
-scanf("%f", &habitantes);
+scanf("%f", &habitantes4);
 
 printf("Informe Área da cidade A04: \n");
-scanf("%f", &area);
+scanf("%f", &area4);
 
 printf("Informe o PIB da cidade A04: \n");
-scanf("%f", &PIB);
+scanf("%f", &PIB4);
 
 printf("Informe a quantidade de pontos turisticos da cidade A04: \n");
-scanf("%d", &turismo);
+scanf("%d", &turismo4);
 printf("\n");
 
 printf("Cidade A01: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes, area, PIB, turismo);
@@ -81,15 +92,15 @@ printf("\n");
 
 
 
-printf("Cidade A02: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes, area, PIB, turismo);
+printf("Cidade A02: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes2, area2, PIB2, turismo2);
 printf("\n");
 
 
-printf("Cidade A03: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes, area, PIB, turismo);
+printf("Cidade A03: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes3, area3, PIB3, turismo3);
 printf("\n");
 
 
-printf("Cidade A04: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes, area, PIB, turismo);
+printf("Cidade A04: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes4, area4, PIB4, turismo4);
 
 
 
