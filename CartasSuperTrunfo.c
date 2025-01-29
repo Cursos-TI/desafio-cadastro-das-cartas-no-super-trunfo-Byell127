60c1af09a7168b99b852e2e7ccfbf65f7b65b155
Byell127 <anthonybyell@gmail.com>
2025-01-29 21:46:42 +0000

    Depois de criar as variaveis fiz algumas alterações para deixar o terminal mais "limpo" para a leitura após o codigo ser executado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -23,73 +23,168 @@ float area4;
 float PIB4;
 int turismo4;
 
-  printf("Digite o nome do seu país: \n");
+float habitantes5;
+float area5;
+float PIB5;
+int turismo5;
+
+float habitantes6;
+float area6;
+float PIB6;
+int turismo6;
+
+float habitantes7;
+float area7;
+float PIB7;
+int turismo7;
+
+float habitantes8;
+float area8;
+float PIB8;
+int turismo8;
+
+  printf("Digite o nome do seu país: ");
   scanf("%s", &nome);
 
-  printf("Seu país tem 8 estados com cada um obtendo 4 cidades. \n");
+  printf("Seu país tem 8 estados com cada um obtendo 4 cidades. ");
   printf("\n");
     
-printf("Vamos começar com o estado A... \n");
+printf("Vamos começar com o estado A... ");
 printf("\n");
 
-printf("Informe a População da cidade A01: \n");
+printf("\n");
+printf("Informe a População da cidade A01: ");
 scanf("%f", &habitantes);
 
-printf("Informe Área da cidade A01: \n");
+printf("Informe Área da cidade A01: ");
 scanf("%f", &area);
 
-printf("Informe o PIB da cidade A01: \n");
+printf("Informe o PIB da cidade A01: ");
 scanf("%f", &PIB);
 
-printf("Informe a quantidade de pontos turisticos da cidade A01: \n");
+printf("Informe a quantidade de pontos turisticos da cidade A01: ");
 scanf("%d", &turismo);
+printf("\n");
 
-printf("Vamos para a cidade A02: \n");
+printf("Vamos para a cidade A02: ");
+printf("\n");
 
-printf("Informe a População da cidade A02 : \n");
+printf("Informe a População da cidade A02 : ");
 scanf("%f", &habitantes2);
 
-printf("Informe Área da cidade A02: \n");
+printf("Informe Área da cidade A02: ");
 scanf("%f", &area2);
 
-printf("Informe o PIB da cidade A02: \n");
+printf("Informe o PIB da cidade A02: ");
 scanf("%f", &PIB2);
 
-printf("Informe a quantidade de pontos turisticos da cidade A02: \n");
+printf("Informe a quantidade de pontos turisticos da cidade A02: ");
 scanf("%d", &turismo2);
+printf("\n");
 
-printf("Vamos para a cidade A03: \n");
+printf("Vamos para a cidade A03: ");
+printf("\n");
 
-printf("Informe a População da cidade A03 : \n");
+printf("Informe a População da cidade A03 : ");
 scanf("%f", &habitantes3);
 
-printf("Informe Área da cidade A03: \n");
+printf("Informe Área da cidade A03: ");
 scanf("%f", &area3);
 
-printf("Informe o PIB da cidade A03: \n");
+printf("Informe o PIB da cidade A03: ");
 scanf("%f", &PIB3);
 
-printf("Informe a quantidade de pontos turisticos da cidade A03: \n");
+printf("Informe a quantidade de pontos turisticos da cidade A03: ");
 scanf("%d", &turismo3);
+printf("\n");
 
-printf("Vamos para a cidade A04: \n");
+printf("Vamos para a cidade A04: ");
+printf("\n");
 
-printf("Informe a População da cidade A04 : \n");
+printf("Informe a População da cidade A04 : ");
 scanf("%f", &habitantes4);
 
-printf("Informe Área da cidade A04: \n");
+printf("Informe Área da cidade A04: ");
 scanf("%f", &area4);
 
-printf("Informe o PIB da cidade A04: \n");
+printf("Informe o PIB da cidade A04: ");
 scanf("%f", &PIB4);
 
-printf("Informe a quantidade de pontos turisticos da cidade A04: \n");
+printf("Informe a quantidade de pontos turisticos da cidade A04: ");
 scanf("%d", &turismo4);
 printf("\n");
 
-printf("Cidade A01: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes, area, PIB, turismo);
+
+printf("Agora vamos para o Estado B com as cidades B01,B02,B03,B04");
 printf("\n");
 
+printf("Informe a População da cidade B01: ");
+scanf("%f", &habitantes5);
+
+printf("Informe Área da cidade B01: ");
+scanf("%f", &area5);
+
+printf("Informe o PIB da cidade B01:");
+scanf("%f", &PIB5);
+
+printf("Informe a quantidade de pontos turisticos da cidade B01:");
+scanf("%d", &turismo5);
+printf("\n");
+
+printf("Vamos para a cidade B02: ");
+printf("\n");
+
+printf("Informe a População da cidade B02 : ");
+scanf("%f", &habitantes6);
+
+printf("Informe Área da cidade B02: ");
+scanf("%f", &area6);
+
+printf("Informe o PIB da cidade B02: ");
+scanf("%f", &PIB6);
+
+printf("Informe a quantidade de pontos turisticos da cidade B02: ");
+scanf("%d", &turismo6);
+printf("\n");
+
+printf("Vamos para a cidade B03:");
+printf("\n");
+
+printf("Informe a População da cidade B03 :");
+scanf("%f", &habitantes7);
+
+printf("Informe Área da cidade B03: ");
+scanf("%f", &area7);
+
+printf("Informe o PIB da cidade B03: ");
+scanf("%f", &PIB7);
+
+printf("Informe a quantidade de pontos turisticos da cidade B03: ");
+scanf("%d", &turismo7);
+printf("\n");
+
+printf("Vamos para a cidade B04: ");
+printf("\n");
+
+printf("Informe a População da cidade B04 : ");
+scanf("%f", &habitantes8);
+
+printf("Informe Área da cidade B04: ");
+scanf("%f", &area8);
+
+printf("Informe o PIB da cidade B04: ");
+scanf("%f", &PIB8);
+
+printf("Informe a quantidade de pontos turisticos da cidade B04: ");
+scanf("%d", &turismo8);
+printf("\n");
+
+
+
+printf("ESTADO A:");
+printf("\n");
+printf("Cidade A01: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes, area, PIB, turismo);
+printf("\n");
 
 
 printf("Cidade A02: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes2, area2, PIB2, turismo2);
@@ -101,8 +196,24 @@ printf("\n");
 
 
 printf("Cidade A04: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes4, area4, PIB4, turismo4);
+printf("\n");
 
-printf("Agora vamos para o Estado B com as cidades B01,B02,B03,B04");
+printf("ESTADO B:");
+printf("\n");
+printf("Cidade B01: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes5, area5, PIB5, turismo5);
+printf("\n");
+
+
+
+printf("Cidade B02: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes6, area6, PIB6, turismo6);
+printf("\n");
+
+
+printf("Cidade B03: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes7, area7, PIB7, turismo7);
+printf("\n");
+
+
+printf("Cidade B04: População %f - Área %f - PIB %f - Ponto(s) Turisticos(s) %d. \n", habitantes8, area8, PIB8, turismo8);
 
 
 
